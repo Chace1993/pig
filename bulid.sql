8c0db48d1713f52ac05126397ccff0c28aaaff6b
Chace1993 <honglaoshi@hotmail.com>
2016-11-11 15:11:55 +0800

    Add files via upload

---
+++ bulid.sql
@@ -0,0 +1,42 @@
+﻿# Host: localhost  (Version: 5.5.40)
+# Date: 2016-11-11 15:14:38
+# Generator: MySQL-Front 5.3  (Build 4.120)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "dept"
+#
+
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept` (
+  `deptno` int(11) DEFAULT NULL,
+  `dname` char(20) DEFAULT NULL,
+  `loc` char(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "emp"
+#
+
+DROP TABLE IF EXISTS `emp`;
+CREATE TABLE `emp` (
+  `empno` int(11) DEFAULT NULL,
+  `ename` char(20) DEFAULT NULL,
+  `job` char(20) DEFAULT NULL,
+  `mgr` int(11) DEFAULT NULL,
+  `hiredate` datetime DEFAULT NULL,
+  `sal` float DEFAULT NULL,
+  `deptno` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "salgrade"
+#
+
+DROP TABLE IF EXISTS `salgrade`;
+CREATE TABLE `salgrade` (
+  `grade` int(11) DEFAULT NULL,
+  `losal` int(11) DEFAULT NULL,
+  `hisal` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
